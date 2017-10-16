9dfdadb19f39cf6d3871a0859b99f71455e12157
Aaron McDaid <aaronmcdaid@gmail.com>
2017-10-16 23:03:53 +0100

    First decent complete version

--- test.random.monad.rkt
+++ test.random.monad.rkt
@@ -1,10 +1,34 @@
 #lang hackett
 
-(require "random.monad.rkt")
+(require "random.monad.rkt") ;; to run this, you might need: "raco make random.monad.rkt  && echo && racket  test.random.monad.rkt"
+
+(def first.prng.script
+    (do
+        [d1 <-  random-double ]
+        (set-seed 1337)
+        [i1 <- (random-integer 50 60)]
+        [i2 <- (random-integer 50 60)]
+        (set-seed 1337)
+        [i3 <- (random-integer 50 60)]
+        (set-seed 1)
+        [d2 <-  random-double ]
+        (pure {i1 tuple d1 tuple i2 tuple i3 tuple d2})))
+
+(defn replicate-random : (forall [a] { Integer -> (PRNG a) -> (PRNG (List a))})
+    [
+        [how-many prng] (if {how-many == 0}
+                            (pure nil)
+                            (do [x <- prng] [xs <- (replicate-random { how-many - 1 } prng) ] (pure {x :: xs}))
+                        )
+    ])
+
+(def one.dice (random-integer 1 7))
+
+(def many.dice : (PRNG (List Integer))
+    (replicate-random 100 one.dice)
+)
 
 (main (do
-    [x <- random-double]
-    [y <- (random-integer 3 8)]
-    (println (show x))
-    (println (show y))
+    { first.prng.script & run-prng & show & println }
+    { many.dice                  & run-prng & show & println }
 ))
